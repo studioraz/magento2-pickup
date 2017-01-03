4f13f6cab79c919ae846125c4bee675fe0b286fa
Itay Raz <itay@studioraz.co.il>
2017-01-03 21:50:18 +0200

    ui component and styling

--- view/frontend/web/js/view/checkout/shipping/ups-ship-block.js
+++ view/frontend/web/js/view/checkout/shipping/ups-ship-block.js
@@ -1,28 +1,39 @@
 define([
     'uiComponent',
+    'uiRegistry',
     'ko',
     'Magento_Checkout/js/model/quote',
     'jquery'
-], function (Component, ko, quote, jQuery) {
+], function (Component, registry, ko, quote, $) {
     'use strict';
     return Component.extend({
         defaults: {
             template: 'SR_UpsShip/checkout/shipping/ups-ship-block'
         },
-        hasPickerInitialized : false,
-        isVisible: ko.observable(false),
-        hasInitialized : false,
-        carrierCode : 'ups_ship',
+        hasPickerInitialized: false,
+        hasInitialized: false,
+        carrierCode: 'ups_ship',
         initialize: function (options) {
 
-            this.initConfig(options);
+            this.registry = registry;
+            this.quote = quote;
 
             quote.shippingMethod.subscribe(this._onShippingMethodChanged, this);
 
+            this._super(options);
+
+            this.observe('isVisible locationHTML hiddenValue');
+
             return this;
+
         },
 
-        _onShippingMethodChanged : function(data) {
+        showPickerPopup: function (data, event) {
+            event.preventDefault();
+            window.PickupsSDK.show();
+        },
+
+        _onShippingMethodChanged: function (data) {
 
             var isActive = data.carrier_code == this.carrierCode;
 
@@ -33,30 +44,59 @@ define([
             }
         },
 
+        _getAdressValue: function (fieldName) {
+            var component = this.registry.get('checkout.steps.shipping-step.shippingAddress.shipping-address-fieldset.' + fieldName);
+            return component ? component.value() : null;
+        },
 
-        _initializePicker : function () {
+        _initializePicker: function () {
 
             // include UPS JS library
             require(['pickups']);
 
-            jQuery(document.body).on('pickups-before-open', function () {
+            $(document.body).on('pickups-before-open', {component: this}, function (event) {
+
+                // prepare customer location
+
+                var self = event.data.component;
+
                 var o = {
                     location: {
-                        city: jQuery('input[name="city"]').val(),
-                        street: jQuery('input[name="street[0]"]').val()
+                        city: self._getAdressValue('city'),
+                        street: self._getAdressValue('street.0')
                     }
                 };
                 var json = JSON.stringify(o);
                 window.PickupsSDK.setDefaults(json);
             });
 
-            jQuery(document.body).on('pickups-after-choosen', function (e, data) {
-                var pkps_location = e.originalEvent.detail;
-                console.log("pickups-after-choosen catched event", pkps_location);
-                jQuery("input[name=shipping_ups_pickup_id]").val(pkps_location.iid);
-                jQuery("input[name=shipping_additional_information]").val(JSON.stringify(pkps_location));
-                var html = "<br /><b>" + pkps_location.title + "</b>&nbsp;(" + pkps_location.iid + ")<br />" + pkps_location.city + ", " + pkps_location.street + "<br /><small>" + pkps_location.zip + "</small>";
-                jQuery("div.ups-pickups-info").css("line-height", "1em").css("font-weight", "300").css("font-size", "0.9em").html(html);
+            $(document.body).on('pickups-after-choosen', {component: this}, function (event, data) {
+
+
+                var location = event.originalEvent.detail;
+
+                /** {
+                     "lat": 32.0876359,
+                     "lng": 34.786194,
+                     "title": "פיקאפ בית מרקחת ברק",
+                     "street": "רמז 15",
+                     "city": "תל אביב יפו",
+                     "zip": "שעות פתיחה א-ה 09:00-19:00 יום ו 09:00-14:00",
+                     "iid": "PKPS639854",
+                     "dist": "0.22"
+                 } **/
+
+                var self = event.data.component;
+
+                var locationInfo =  _.values(_.pick(location, 'title', 'street', 'city' ,'zip', 'iid'));
+
+                self.locationHTML(locationInfo.join(', '));
+
+
+                self.hiddenValue(JSON.stringify(location));
+
+
+
             });
 
             this.hasPickerInitialized = true;
